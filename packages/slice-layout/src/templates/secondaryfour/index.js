ff4d187d40ed80c674d948d8f9a3d2ac3f819d04
NehaSrivastava <neha.srivastava84525@gmail.com>
2019-01-25 18:36:57 +0000

    feat: working slice

--- packages/slice-layout/src/templates/secondaryfour/index.js
+++ packages/slice-layout/src/templates/secondaryfour/index.js
@@ -1,43 +1,7 @@
 import React from "react";
 import { View, Text } from "react-native";
-// import styles from "../styles";
+import styles from "./styles";
 
-// const WebSyles = {
-//   itemContainer: {
-//     flex:1,
-//     flexDirection:'row',
-//     borderBottomWidth: 1,
-//     borderStyle: "solid",
-//     backgroundColor: "green",
-//     // minHeight: 150,
-//     // width:"100%"
-//   },
-//   item: {
-//     flex:1,
-//     borderBottomWidth: 1,
-//     borderStyle: "solid",
-//     backgroundColor: "blue",
-//     // minHeight: 150,
-//     // width:"50%"
-//   }
-// };
-
-const styles = {
-  itemContainer: {
-    flex:1,
-    flexDirection:'row',
-    borderBottomWidth: 1,
-    borderStyle: "solid",
-    backgroundColor: "green",
-  },
-  item: {
-    // flex:1,
-    borderBottomWidth: 1,
-    borderStyle: "solid",
-    backgroundColor: "blue",
-    width:"50%"
-  }
-};
 const SecondaryFourSlice = ({
   renderSecondary1,
   renderSecondary2,
@@ -48,18 +12,18 @@ const SecondaryFourSlice = ({
     const renderSecondaryRowOne = [renderSecondary1(), renderSecondary2()];
     const renderSecondaryRowTwo = [renderSecondary3(), renderSecondary4()];
     return (
-      <View style={styles.container}>
-        <View style={styles.itemContainer}>
-          {renderSecondaryRowOne.map(support => (
-            <View style={styles.item} key={support.props.id}>{support}</View>
-          ))}
+        <View style={styles.container}>
+          <View style={styles.itemContainer}>
+            {renderSecondaryRowOne.map(support => (
+              <View style={styles.item} key={support.props.id}>{support}</View>
+            ))}
+          </View>
+          <View style={styles.itemContainer}>
+            {renderSecondaryRowTwo.map(support => (
+              <View style={styles.item} key={support.props.id}>{support}</View>
+            ))}
+          </View>
         </View>
-        <View style={styles.itemContainer}>
-          {renderSecondaryRowTwo.map(support => (
-            <View style={styles.item} key={support.props.id}>{support}</View>
-          ))}
-        </View>
-      </View>
       );
   };
 
---
+++ packages/slice-layout/src/templates/secondaryfour/styles.js
@@ -0,0 +1,21 @@
+const styles = {
+    container: {
+      flex: 1,
+      height: 1,
+    },
+    itemContainer: {
+      flexDirection:'row',
+      borderBottomWidth: 0.5,
+      borderStyle: "solid",
+      margin: 5
+    },
+    item: {
+      width:"50%",
+      padding: 5,
+      borderLeftWidth: 0.5,
+      borderRightWidth: 0.5,
+      borderStyle: "solid"
+    }
+  };
+
+export default styles;
